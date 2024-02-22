7a2fbf4158839c1d82be7e0f647160339d0e7385
LinCanhui <lingyun2311@gmail.com>
2024-02-23 01:35:07 +0800

    整合 MyBatis 和 MyBatisPlus，并添加配置类

---
+++ src/main/java/com/mlinyun/onlinecourse/basics/mybatisplus/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.mlinyun.onlinecourse.basics.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import io.swagger.v3.oas.annotations.Operation;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus 配置类
+ */
+@Configuration
+@MapperScan({"com.mlinyun.onlinecourse.*.*.mapper"})
+public class MybatisPlusConfig {
+
+    @Bean
+    @Operation(description = "初始化MybatisPlus配置")
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
